cb26f3707f03854069a48bfb4b540ffead99e852
I Putu Hariyadi <iputu_hariyadi@yahoo.com>
2021-08-14 18:14:33 +0000

    Use a Python script to test your access token

--- authentication.py
+++ authentication.py
@@ -1,7 +1,7 @@
 # Fill in this file with the authentication code from the Webex Teams exercise
 import requests
 import json
-access_token = 'MWNhMjU3NWUtZmZiYy00YjExLTk3NTktN2FmMWFlMzcwYTE1YjEzNGEyZmQtMzg3_P0A1_c1cccb6a-5e2d-4f80-86f9-f0dcefbd12f8'
+access_token = 'your_token_here'
 url = 'https://webexapis.com/v1/people/me'
 headers = {
     'Authorization': 'Bearer {}'.format(access_token)
