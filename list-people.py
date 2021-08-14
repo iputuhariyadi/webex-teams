622b9c3127e161ee1354df3a2a6f4deafd897435
I Putu Hariyadi <iputu_hariyadi@yahoo.com>
2021-08-14 18:16:09 +0000

    Use a Python scrip to list details of a registered Webex Teams user.

--- list-people.py
+++ list-people.py
@@ -2,19 +2,19 @@
 import requests
 import json
 
-access_token = 'MWNhMjU3NWUtZmZiYy00YjExLTk3NTktN2FmMWFlMzcwYTE1YjEzNGEyZmQtMzg3_P0A1_c1cccb6a-5e2d-4f80-86f9-f0dcefbd12f8'
+access_token = 'your_token_here'
 url = 'https://webexapis.com/v1/people'
 headers = {
     'Authorization': 'Bearer {}'.format(access_token),
     'Content-Type': 'application/json'
 }
 params = {
-    'email': 'putu.hariyadi@universitasbumigora.ac.id'
+    'email': 'user@example.com'
 }
 resp = requests.get(url, headers=headers, params=params)
 print(json.dumps(resp.json(), indent=4))
 
-person_id = 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS9mZDQ3ODJmMi0yZjgzLTQ4NGYtYmQxOS1kNzhlNmM2NTZhOWI'
+person_id = 'previous_id'
 url = 'https://webexapis.com/v1/people/{}'.format(person_id)
 resp = requests.get(url, headers=headers)
 print(json.dumps(resp.json(), indent=4))
