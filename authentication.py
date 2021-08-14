4d49ad505c987a1476b283e44c718770828a79c1
I Putu Hariyadi <iputu_hariyadi@yahoo.com>
2021-08-14 17:57:33 +0000

    List additional administrative details for a person

--- authentication.py
+++ authentication.py
@@ -1,1 +1,10 @@
 # Fill in this file with the authentication code from the Webex Teams exercise
+import requests
+import json
+access_token = 'MWNhMjU3NWUtZmZiYy00YjExLTk3NTktN2FmMWFlMzcwYTE1YjEzNGEyZmQtMzg3_P0A1_c1cccb6a-5e2d-4f80-86f9-f0dcefbd12f8'
+url = 'https://webexapis.com/v1/people/me'
+headers = {
+    'Authorization': 'Bearer {}'.format(access_token)
+}
+res = requests.get(url, headers=headers)
+print(json.dumps(res.json(), indent=4))
--- list-people.py
+++ list-people.py
@@ -1,1 +1,21 @@
 # Fill in this file with the people listing code from the Webex Teams exercise
+import requests
+import json
+
+access_token = 'MWNhMjU3NWUtZmZiYy00YjExLTk3NTktN2FmMWFlMzcwYTE1YjEzNGEyZmQtMzg3_P0A1_c1cccb6a-5e2d-4f80-86f9-f0dcefbd12f8'
+url = 'https://webexapis.com/v1/people'
+headers = {
+    'Authorization': 'Bearer {}'.format(access_token),
+    'Content-Type': 'application/json'
+}
+params = {
+    'email': 'putu.hariyadi@universitasbumigora.ac.id'
+}
+resp = requests.get(url, headers=headers, params=params)
+print(json.dumps(resp.json(), indent=4))
+
+person_id = 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS9mZDQ3ODJmMi0yZjgzLTQ4NGYtYmQxOS1kNzhlNmM2NTZhOWI'
+url = 'https://webexapis.com/v1/people/{}'.format(person_id)
+resp = requests.get(url, headers=headers)
+print(json.dumps(resp.json(), indent=4))
+
