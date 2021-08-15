a723ec4e4dc54b4124b51b9760c5ea0b6d90f116
I Putu Hariyadi <iputu_hariyadi@yahoo.com>
2021-08-15 06:47:22 +0000

    Use a Python script to list all the rooms for an authenticated user

--- list-rooms.py
+++ list-rooms.py
@@ -1,1 +1,12 @@
 # Fill in this file with the rooms/spaces listing code from the Webex Teams exercise
+import requests
+
+access_token = 'your_token_here'
+url = 'https://webexapis.com/v1/rooms'
+headers = {
+    'Authorization': 'Bearer {}'.format(access_token),
+    'Content-Type': 'application/json'
+}
+params = {'max': '100'}
+res = requests.get(url, headers=headers, params=params)
+print(res.json())
