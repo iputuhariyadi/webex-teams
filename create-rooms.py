ce5e31663ad90be5e75c9e6eb7dcf2242ef7cf47
I Putu Hariyadi <iputu_hariyadi@yahoo.com>
2021-08-15 06:48:02 +0000

    Use a Python script to create a room

--- create-rooms.py
+++ create-rooms.py
@@ -1,1 +1,12 @@
 # Fill in this file with the code to create a new room from the Webex Teams exercise
+import requests
+
+access_token = 'your_token_here'
+url = 'https://webexapis.com/v1/rooms'
+headers = {
+    'Authorization': 'Bearer {}'.format(access_token),
+    'Content-Type': 'application/json'
+}
+params = {'title': 'DevNet Associate Training!'}
+res = requests.post(url, headers=headers, json=params)
+print(res.json())
